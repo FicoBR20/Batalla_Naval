75542a3f89fe8b9a613a36ffa67220b02bfa2e45
federico <federico.barbetti@correounivalle.edu.co>
2023-06-29 11:51:28 -0500

    se especifican variables en clase armamento

--- src/modelo/Armamento.java
+++ src/modelo/Armamento.java
@@ -25,7 +25,7 @@ public class Armamento {
     /**
      * atributo integrador de las
      * naves de combate y entes.
-     * recepciona un Array de carroceria.
+     * recepciona los atributos.
      */
     private LinkedList<Object>fuselaje;
     /**
@@ -34,18 +34,19 @@ public class Armamento {
      * 0 -> es agua
      * 1 -> es metal
      * 2 -> fue tocado o impactado por el rival.
+     * ejemplo: [0,1,1,1,0] seria el submarino
      */
     private ArrayList<Integer>carroceria;
     /**
      * atributo que determina la rotacion
-     * de una nave
+     * de una nave. El valor default es false
      */
     private Boolean is_rotate;
     /**
      * atributo para asignacion del icono
      * representativo de la nave
      */
-    private int Icon;
+    private int icono_asociado;
     /**
      * Atributo que asigna la distancia minima
      * entre los entes presentes en la batalla.
@@ -53,7 +54,7 @@ public class Armamento {
     private int espacio_libre;
     /**
      * atributo que determina el limite de supervivencia
-     * de una nave
+     * de una nave, el default es 100% se impactaron toda la nave
      */
     private double nivel_de_impactos;
 }
